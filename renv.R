af2ad3d0fc62e7b93dc27acfacccf73cba0fc913
Nils Ratnaweera <rata@zhaw.ch>
2021-01-04 13:03:17 +0100

    trying to force the use of songbookdown

--- renv.R
+++
@@ -1,3 +0,0 @@
-# The sole purpose of this file is to make sure that I can capture dependencies not detected by renv. 
-# Todo: create a description file (https://rstudio.github.io/renv/articles/faq.html#capturing-explicit-dependencies-1)
-library(songbookdown)
