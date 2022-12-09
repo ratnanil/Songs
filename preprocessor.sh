eddfe20a53e5514599a1835d470d54b114d904c7
Nils Ratnaweera <rata@zhaw.ch>
2022-12-09 23:27:19 +0100

    update prepro

--- preprocessor.sh
+++ preprocessor.sh
@@ -1,14 +1,14 @@
-echo "try to remove any left over md files"
-rm */*.md
-echo "replace consecutive spaces with a space and a nbsp"
+# echo "try to remove any left over md files"
+# rm */*.md
+# echo "replace consecutive spaces with a space and a nbsp"
 for d in */*.qmd; 
-    do sed $'s/\u0020\u0020/\u0020\u00a0/g' $d > ${d%%.*}.md;
+    do sed -i $'s/\u0020\u0020/\u0020\u00a0/g' $d;
 done
-echo "now render quarto"
-quarto render
-quarto publish gh-pages --no-prompt
-echo "now remove all md files"
-rm */*.md
+# echo "now render quarto"
+# quarto render
+# quarto publish gh-pages --no-prompt
+# echo "now remove all md files"
+# rm */*.md
 # perl -i.bak -p -e 's/\x{0020}\x{0020}/\x{0020}\x{00a0}/g' *.qmd
 # perl -i.bak -p -e "Encode::from_to($_, 'Windows-1255', 'utf-8')" index.qmd
 # quarto render
