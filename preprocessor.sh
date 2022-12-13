623c742919d1a75c09b817ccd52959ab1feef58e
Nils Ratnaweera <rata@zhaw.ch>
2022-12-13 22:20:44 +0100

    newest version of preprocessor

--- preprocessor.sh
+++ preprocessor.sh
@@ -1,17 +1,4 @@
-# echo "try to remove any left over md files"
-# rm */*.md
-# echo "replace consecutive spaces with a space and a nbsp"
-for d in */*.qmd; 
-    do sed -i $'s/\u0020\u0020/\u0020\u00a0/g' $d;
+for d in */*.qmd; do 
+    # sed $'s/\u0020/\./g' $d
+    perl -i -p -e "s/\x{0020}\x{0020}/\x{0020}\x{00a0}/g;" $d 
 done
-# echo "now render quarto"
-# quarto render
-# quarto publish gh-pages --no-prompt
-# echo "now remove all md files"
-# rm */*.md
-# perl -i.bak -p -e 's/\x{0020}\x{0020}/\x{0020}\x{00a0}/g' *.qmd
-# perl -i.bak -p -e "Encode::from_to($_, 'Windows-1255', 'utf-8')" index.qmd
-# quarto render
-#for file in *.qmd.bak; do cp "$file" "${file//.bak}"; done
-# rm *.qmd
-# rename "s/\.bak//" *.bak -v -d 
