aa70a299e5a7678bb0b510e055825c7cd89f37e6
Nils Ratnaweera <rata@zhaw.ch>
2023-01-16 07:48:13 +0100

    replace spaces at the beginning of a line with a no-break-space

--- preprocessor.sh
+++ preprocessor.sh
@@ -4,5 +4,6 @@ for d in */*.qmd; do
     perl -i -p -e "s/  /  /g;" $d #searching for \u0020\u0020 and replacing with \u00a0\u00a0
     perl -i -p -e "s/  /  /g;" $d #searching for \u0020\u00a0 and replacing with \u00a0\u00a0
     perl -i -p -e "s/  /  /g;" $d #searching for \u00a0\u0020 and replacing with \u00a0\u00a0
+    perl -i -p -e "s/^ / /g;" $d  #searching for u0020 at the beginning of a line and replacing it with \u00a0
 
 done
