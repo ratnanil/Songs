c70fbd9b0335db92a8cf241793b1b5db7d5b0c7d
Nils Ratnaweera <rata@zhaw.ch>
2023-01-26 15:12:02 +0100

    minor issue with my preprocessor

--- preprocessor.sh
+++ preprocessor.sh
@@ -1,7 +1,8 @@
 echo preprocessing
 for d in */*.qmd; do 
     # determin the line number of the YAML terminator (second ---)
-    line=$(grep -n "^---" index.qmd | head -2 | tail -1 | awk -F: '{print $1}' )
+    line=$(grep -n "^---" $d | head -2 | tail -1 | awk -F: '{print $1}' )
+    echo $d $line
     # replace various form of double spaces wtih non-breaking spaces. BUT: skip the YAML header. 
     perl -i -sne 'if ($. < $n) { print } else { s/  /  /g; print }' -- -n=$line $d
     perl -i -sne 'if ($. < $n) { print } else { s/  /  /g; print }' -- -n=$line $d
