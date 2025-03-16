e1a786142f3f4cf2853dcfd855bb7aac5747bfd3
ratnanil <rata@zhaw.ch>
2025-03-16 23:41:06 +0100

    update

--- preprocessor.sh
+++ preprocessor.sh
@@ -1,11 +1,18 @@
 echo preprocessing
+
+# for all qmd files in subdirectories...
 for d in */*.qmd; do 
-    # determin the line number of the YAML terminator (second ---)
+    # ...determine the line number of the YAML terminator (second "---")
     line=$(grep -n "^---" $d | head -2 | tail -1 | awk -F: '{print $1}' )
     echo $d $line
-    # replace various form of double spaces wtih non-breaking spaces. BUT: skip the YAML header. 
-    perl -i -sne 'if ($. < $n) { print } else { s/  /  /g; print }' -- -n=$line $d
-    perl -i -sne 'if ($. < $n) { print } else { s/  /  /g; print }' -- -n=$line $d
-    perl -i -sne 'if ($. < $n) { print } else { s/  /  /g; print }' -- -n=$line $d
-    perl -i -sne 'if ($. < $n) { print } else { s/^ / /g; print }' -- -n=$line $d
+    # ...replace various form of double spaces wtih non-breaking spaces. BUT: skip the YAML header. 
+    perl -i -sne 'if ($. < $n) { print } else { s/  /  /g; print }' -- -n=$line $d  # double spaces
+    perl -i -sne 'if ($. < $n) { print } else { s/  /  /g; print }' -- -n=$line $d  # space + non-breaking space
+    perl -i -sne 'if ($. < $n) { print } else { s/  /  /g; print }' -- -n=$line $d  # non-breaking space + space
+    perl -i -sne 'if ($. < $n) { print } else { s/^ / /g; print }' -- -n=$line $d   # space at beginning of line
+    # -i is in-place edit
+    # -s is to pass arguments to the script ($n in this case, which is assigned after the --)
+    # -n is the line number of the YAML terminator
+    # -e is to pass a script to the perl interpreter
 done
+
